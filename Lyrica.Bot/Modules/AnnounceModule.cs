4dda0d5152d69bc651a4c9612ec2020fa44f879b
hizamakura <kaworinakano@gmail.com>
2020-08-14 08:09:48 +0800

    Improve announcement module
    - Make sure to pick the right image
    - Fix invalid author url

---
+++ Lyrica.Bot/Modules/AnnounceModule.cs
@@ -0,0 +1,96 @@
+﻿using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Lyrica.Services.Core.Attributes;
+using Lyrica.Services.Interactive;
+using Lyrica.Services.WebHooks;
+using static Lyrica.Bot.Modules.AnnounceModule.StreamPingArguments;
+
+namespace Lyrica.Bot.Modules
+{
+    [Group("announce")]
+    [Summary("Group of commands of announcing various pre-formatted embeds")]
+    public class AnnounceModule : InteractivePromptBase
+    {
+        public enum StreamPingArguments
+        {
+            Description,
+            Author,
+            ImageUrl,
+            Footer
+        }
+
+        private readonly Regex _tweetRegex;
+
+        private readonly TwitterService _twitterService;
+
+        public AnnounceModule(TwitterService twitterService)
+        {
+            _twitterService = twitterService;
+            _tweetRegex = new Regex(@"
+                (https?://)?twitter\.com/(?<user>\w{1,15})/status/(?<tweetId>[0-9]+)
+                (/photo/(?<photoNumber>([1-9][0-9]*)))?",
+                RegexOptions.Compiled | RegexOptions.IgnorePatternWhitespace);
+        }
+
+        public async Task StreamPingAsync(ulong tweetId, [Remainder] string title)
+        {
+            var prompts = new PromptCollection<StreamPingArguments>(this)
+                .WithPrompt(Description, "Enter the message of the announcement");
+        }
+
+        [Command("stream")]
+        public async Task StreamPingAsync(string tweetLink, [Remainder] string? message = null)
+        {
+            var embed = new EmbedBuilder()
+                .WithFooter("Stream starts at", "https://abs.twimg.com/icons/apple-touch-icon-192x192.png");
+            var tweet = _tweetRegex.Match(tweetLink);
+
+            if (!tweet.Success)
+            {
+                await ReplyAsync("That's an invalid tweet!");
+                return;
+            }
+
+            if (!ulong.TryParse(tweet.Groups["tweetId"].Value, out var tweetId))
+            {
+                await ReplyAsync("The tweet number is invalid!");
+                return;
+            }
+
+            if (tweet.Groups["photoNumber"].Success)
+            {
+                var photoNumber = int.Parse(tweet.Groups["photoNumber"].Value);
+                await _twitterService.AddTweetInfo(embed, tweetId, photoNumber - 1);
+            }
+            else
+            {
+                await _twitterService.AddTweetInfo(embed, tweetId);
+            }
+
+            await ReplyAsync(message, embed: embed.Build());
+        }
+
+        [Command("test")]
+        public async Task TestAsync(ulong tweetId)
+        {
+            var embed = new EmbedBuilder()
+                .WithFooter("Stream starts at", "https://abs.twimg.com/icons/apple-touch-icon-192x192.png");
+
+            await _twitterService.AddTweetInfo(embed, tweetId);
+
+            await ReplyAsync("Test", embed: embed.Build());
+        }
+
+        [NamedArgumentType]
+        public class AnnouncementArguments
+        {
+            [Description("Text Channel to post the ping in")]
+            public ITextChannel? Channel { get; set; } = null!;
+
+            [Description("This is the overriden title")]
+            public string? Title { get; set; } = null!;
+        }
+    }
+}
--- Lyrica.Services/Webhooks/TwitterService.cs
+++ Lyrica.Services/Webhooks/TwitterService.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web;
 using Discord;
 using LinqToTwitter;
 using Lyrica.Data.Config;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Color = Discord.Color;
 
 namespace Lyrica.Services.WebHooks
 {
@@ -33,25 +35,39 @@ namespace Lyrica.Services.WebHooks
             _twitterClient = new TwitterContext(auth);
         }
 
-        public async Task<EmbedBuilder> AddInformation(EmbedBuilder builder, ulong tweetId)
+        public async Task<EmbedBuilder> AddTweetInfo(EmbedBuilder builder, ulong tweetId, int? photoNumber = null)
         {
-            var status = await _twitterClient.Status
+            var status = await GetTweet(tweetId);
+
+            if (status is null)
+                throw new ArgumentOutOfRangeException(nameof(tweetId), "The tweet does not exist");
+
+            var statusLink = $"https://twitter.com/{status.User.ScreenNameResponse}/status/{status.ID}";
+
+            builder
+                .WithDescription(HttpUtility.HtmlDecode(status.FullText))
+                .WithColor((Color) ColorTranslator.FromHtml($"#{status.User.ProfileLinkColor}"))
+                .WithAuthor(status.User.Name, status.User.ProfileImageUrl, statusLink);
+
+            if (photoNumber > status.Entities.MediaEntities.Count)
+                photoNumber = status.Entities.MediaEntities.Count;
+            else if (photoNumber < 0)
+                photoNumber = 0;
+
+            if (status.Entities.MediaEntities.Any())
+                builder.WithImageUrl(status.Entities.MediaEntities[photoNumber ?? 0].MediaUrl);
+
+            return builder;
+        }
+
+        private async Task<Status> GetTweet(ulong tweetId)
+        {
+            return await _twitterClient.Status
                 .Where(tweet =>
                     tweet.Type == StatusType.Show &&
                     tweet.ID == tweetId &&
                     tweet.TweetMode == TweetMode.Extended)
-                .Include(x => x.Entities)
                 .FirstOrDefaultAsync();
-
-            if (status is null)
-                throw new ArgumentNullException(nameof(status));
-
-            builder
-                .WithDescription(status.FullText)
-                .WithImageUrl(status.Entities.MediaEntities.First().MediaUrl)
-                .WithAuthor(status.User.ScreenName, status.User.ProfileImageUrl, status.Entities.UrlEntities[0].Url);
-
-            return builder;
         }
     }
 }
